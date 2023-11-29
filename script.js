79d41c422cdb944b17667ebe5435ab2d33b8eebe
Meng Li <MengLi@mediaworks.co.nz>
2023-11-30 12:10:05 +1300

    another commit

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
-function adBlockDetector() {
-    setTimeout(function(){
-        document.body.innerHTML += '<div class="adsbygoogle" id="test-ad"></div>'
-        const testAd = document.getElementById('test-ad')
-        const testAdStyle = getComputedStyle(testAd)
+// function adBlockDetector() {
+//     setTimeout(function(){
+//         document.body.innerHTML += '<div class="adsbygoogle" id="test-ad"></div>'
+//         const testAd = document.getElementById('test-ad')
+//         const testAdStyle = getComputedStyle(testAd)
 
-        if(testAdStyle.display==='none'){
-            console.log('ad blocker detected!')
-        } else{
-            console.log('No ad blocker detected!')
-        }
+//         if(testAdStyle.display==='none'){
+//             console.log('ad blocker detected!')
+//         } else{
+//             console.log('No ad blocker detected!')
+//         }
 
-    }, 3000)
-}
+//     }, 3000)
+// }
 
-adBlockDetector()
+// adBlockDetector()
 
-window.addEventListener('load', function(){
+// window.addEventListener('load', function(){
+//     fetch('https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js')
+//     .then(res=> console.log('ads allowed'))
+//     .catch(err=>console.log('adblock detected!'))
+// })
+
+
     fetch('https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js')
     .then(res=> console.log('ads allowed'))
-    .catch(err=>console.log('adblock detected!'))
-})
+    .catch(err=> {
+        console.log('adblock detected!')
+        alert('adblock detected!')
+    })
