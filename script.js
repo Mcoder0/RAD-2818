df963e2b196d2ccabd088a1d08c30642be863d60
Meng Li <MengLi@mediaworks.co.nz>
2023-11-30 11:29:27 +1300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function adBlockDetector() {
+    setTimeout(function(){
+        document.body.innerHTML += '<div class="adsbygoogle" id="test-ad"></div>'
+        const testAd = document.getElementById('test-ad')
+        const testAdStyle = getComputedStyle(testAd)
+
+        if(testAdStyle.display==='none'){
+            console.log('ad blocker detected!')
+        } else{
+            console.log('No ad blocker detected!')
+        }
+
+    }, 1000)
+}
+
+adBlockDetector()
